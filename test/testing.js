5f3333f371dec0862a3b81f63bb12bdc1b3d0582
torbjorn josefsson <torbjorn.josefsson@gmail.com>
2013-04-03 00:10:08 +0200

    blah

--- test/testing.js
+++ test/testing.js
@@ -1,16 +1,11 @@
-var MarkdownStream  = require('./markdown-stream'),
+var MarkdownStream  = require('../markdown-stream'),
     fs              = require('fs');
 
-var fileSize = 0;
 fs.stat('test.text', function (err, stats) {
- console.log(stats.size);
-stats.size = fileSize;
+  var fstream = fs.createReadStream('test.text',{bufferSize: stats.size});
+  var parser = new MarkdownStream();
+  var outstream = fs.createWriteStream("test.html");
+  fstream.pipe(parser).pipe(outstream);
 });
 
-var fstream = fs.createReadStream('test.text',{bufferSize: fileSize});
-console.dir(fstream);
-var parser = new MarkdownStream();
-var outstream = fs.createWriteStream("test.html");
 
-// now pump some data into it (and pipe it somewhere else)
-fstream.pipe(parser).pipe(outstream);
